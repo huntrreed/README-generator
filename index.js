1a82fb5d4e6213f3e6d604557c965611ad805309
huntrreed <146265520+huntrreed@users.noreply.github.com>
2023-12-18 16:28:14 -0700

    packages and one question written

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    { 
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of the project?' 
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README.md, data) {
+    fs.writeFile(README.md, data, (error!) =>
+    err ? console.error(error!) : console.log('README.md generated!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions)
+    .then((answers)=> {
+        const readmeContent = generateReadmeContent(answers);
+        writeToFile('README.md', readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
