ae8fb0b006d5b5e2e87980dc08125c97ca186da8
huntrreed <146265520+huntrreed@users.noreply.github.com>
2023-12-19 14:34:18 -0700

    added more questions,broke the init function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
@@ -8,19 +8,50 @@ const questions = [
     type: 'input',
     name: 'title',
     message: 'What is the title of the project?' 
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description of the project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install the project?'
+    },
+    {
+        type: 'input',
+        name: 'usage information',
+        message: 'How do is your application used?'
+    },
+    {
+        type: 'input',
+        name: 'contribution guidelines',
+        message: 'What are the guidelines for your project?'
+    },
+    {
+        type: 'input',
+        name: 'test instructions',
+        message: 'How do you test the project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license from the following list for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None'],
     }
-
 ];
+//checking that questions are loading correctly
 console.log(questions);
 
-// TODO: Create a function to write README file
+// A function to write README file
 function writeToFile(ReadMeGen, data) {
     fs.writeFile(ReadMeGen, data, (err) =>
     err ? console.error(err) : console.log('README.md generated!')
     );
 }
 
-// TODO: Create a function to initialize app
+//Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then((answers)=> {
@@ -32,5 +63,6 @@ function init() {
 // Function call to initialize app
 function generateReadmeContent(answers){
     return
-}
+
+    
 init();
